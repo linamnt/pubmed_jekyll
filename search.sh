14d1b8df88c16c5cc872234ad34b43f23f1162ca
Lina Tran <lina.mntran@gmail.com>
2018-04-16 15:14:07 -0400

    update instructions in shell

--- search.sh
+++ search.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+# edit the following
 EMAIL='email@domain.ca'
 AUTHOR="LASTNAME INITIALS"
 MAX_SEARCH=20
 
-#check if _data/ folder exists, otherwise create it
+# checks if _data/ folder exists, otherwise create it
 mkdir -p _data
-#add -d to NOT populate the datafiles, -v to print out verbose abstracts into STDOUT
+# add -d to prevent populating of datafiles, -v to print out verbose abstracts
+# into STDOUT
 python pubmed_author_papers.py $EMAIL "$AUTHOR" --max $MAX_SEARCH -d -v
